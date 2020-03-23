33f9701d388fdea16bdfdc74efac719f6531fea6
Michael <13236924+Mirobit@users.noreply.github.com>
2020-03-23 17:51:19 +0100

    JS renaming

--- server/public/js/editor.js
+++ server/public/js/editor.js
@@ -1,42 +1,40 @@
 var text =
-  ".there Far far away, behind the word mountains, far from countries Vokalia the there and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the, coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia."
-// text = "<span>" + text
-// text = text.replace(" ", "</span> <span>")
-const textEditiorDiv = document.getElementById("texteditor")
+  '.there Far far away, behind the word mountains, far from countries Vokalia the there and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the, coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.'
+const textEditiorDiv = document.getElementById('texteditor')
 textEditiorDiv.innerHTML = text
 
 labels = [
   {
     keyCode: 80,
-    keyString: "P",
-    name: "Person",
-    color: "primary",
-    colorHex: "#007BFF"
+    keyString: 'P',
+    name: 'Person',
+    color: 'primary',
+    colorHex: '#007BFF'
   },
   {
     keyCode: 87,
-    keyString: "W",
-    name: "Place",
-    color: "info",
-    colorHex: "#17A2B8"
+    keyString: 'W',
+    name: 'Place',
+    color: 'info',
+    colorHex: '#17A2B8'
   },
   {
     keyCode: 67,
-    keyString: "C",
-    name: "Company",
-    color: "secondary",
-    colorHex: "#6C757D"
+    keyString: 'C',
+    name: 'Company',
+    color: 'secondary',
+    colorHex: '#6C757D'
   },
   {
     keyCode: 79,
-    keyString: "o",
-    name: "Other",
-    color: "dark",
-    colorHex: "#343A40"
+    keyString: 'o',
+    name: 'Other',
+    color: 'dark',
+    colorHex: '#343A40'
   }
 ]
 
-document.addEventListener("keydown", event => {
+document.addEventListener('keydown', event => {
   const selectedLabel = labels.find(
     element => element.keyCode === event.keyCode
   )
@@ -55,16 +53,16 @@ function clickWord() {
   const range = selection.getRangeAt(0)
 
   // Why??
-  if (node.parentElement.className !== "texteditor") return
+  if (node.parentElement.className !== 'texteditor') return
 
   while (range.startOffset >= 0) {
     const firstChar = range.toString().charAt(0)
     if (
-      firstChar === " " ||
-      firstChar === "," ||
-      firstChar === "." ||
-      firstChar === ";" ||
-      firstChar === ":"
+      firstChar === ' ' ||
+      firstChar === ',' ||
+      firstChar === '.' ||
+      firstChar === ';' ||
+      firstChar === ':'
     ) {
       range.setStart(node, range.startOffset + 1)
       break
@@ -79,11 +77,11 @@ function clickWord() {
   while (range.endOffset <= text.length) {
     const lastChar = range.toString().slice(-1)
     if (
-      lastChar === " " ||
-      lastChar === "," ||
-      lastChar === "." ||
-      lastChar === ";" ||
-      lastChar === ":"
+      lastChar === ' ' ||
+      lastChar === ',' ||
+      lastChar === '.' ||
+      lastChar === ';' ||
+      lastChar === ':'
     ) {
       range.setEnd(node, range.endOffset - 1)
       break
@@ -105,18 +103,18 @@ function addLable(label) {
   range.deleteContents()
 
   // Create elements for labeled area
-  var span = document.createElement("span")
-  span.classList.add("labeledarea")
-  var spanLabel = span.appendChild(document.createElement("span"))
-  spanLabel.classList.add("labeled")
+  var span = document.createElement('span')
+  span.classList.add('labeledarea')
+  var spanLabel = span.appendChild(document.createElement('span'))
+  spanLabel.classList.add('labeled')
   spanLabel.innerText = label.name
-  spanLabel.style = "background-color:" + label.colorHex
-  var spanOriginal = span.appendChild(document.createElement("span"))
-  spanOriginal.classList.add("originalWord")
+  spanLabel.style = 'background-color:' + label.colorHex
+  var spanOriginal = span.appendChild(document.createElement('span'))
+  spanOriginal.classList.add('originalWord')
   spanOriginal.hidden = true
   spanOriginal.innerText = selected
-  var spanRemove = span.appendChild(document.createElement("span"))
-  spanRemove.classList.add("removeInit")
+  var spanRemove = span.appendChild(document.createElement('span'))
+  spanRemove.classList.add('removeInit')
 
   // Insert created element and remove selection
   range.insertNode(span)
@@ -139,28 +137,28 @@ function addLabelsGlobal(labelName, labelColor, selected) {
     labelName +
     '</span><span class="confirm" onclick="confirmLabel(this)"></span><span class="remove" onclick="removeLabel(this)"></span></span>'
   textEditiorDiv.innerHTML = textEditiorDiv.innerHTML.replace(
-    new RegExp("((?!>).)\\b" + selected + "\\b", "g"),
+    new RegExp('((?!>).)\\b' + selected + '\\b', 'g'),
     confirmHTML
   )
   // Necessary since all previously set eventlisteners are remove during innerHTMLreplace
   textEditiorDiv.innerHTML = textEditiorDiv.innerHTML.replace(
-    new RegExp('<span class="removeInit"></span>', "g"),
+    new RegExp('<span class="removeInit"></span>', 'g'),
     '<span class="remove" onclick="removeLabel(this)"></span>'
   )
 }
 
 function confirmLabel(element) {
   const parent = element.parentElement
-  const divider = parent.getElementsByClassName("confirmDivider")[0]
+  const divider = parent.getElementsByClassName('confirmDivider')[0]
   divider.remove()
   element.remove()
-  parent.getElementsByClassName("originalWord")[0].hidden = true
+  parent.getElementsByClassName('originalWord')[0].hidden = true
   window.getSelection().removeAllRanges()
 }
 
 function removeLabel(element) {
   const parent = element.parentElement
-  const originalWord = parent.getElementsByClassName("originalWord")[0]
+  const originalWord = parent.getElementsByClassName('originalWord')[0]
     .innerText
   textEditiorDiv.insertBefore(document.createTextNode(originalWord), parent)
   parent.remove()
