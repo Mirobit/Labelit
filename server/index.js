99682558bba39b7a9949816a33ebffc956754754
Michael <13236924+Mirobit@users.noreply.github.com>
2020-03-23 11:36:26 +0100

    Clean up

--- server/index.js
+++ server/index.js
@@ -1,29 +1,24 @@
 const express = require("express")
 const path = require("path")
-const mongoose = require("mongoose");
-const fileUpload = require("express-fileupload")
+const mongoose = require("mongoose")
 const routes = require("./routes")
 
-const MONGODB_URI = 'mongodb://localhost/lableit'
+const MONGODB_URI = "mongodb://localhost/lableit"
 
-
-mongoose.Promise = Promise;
+mongoose.Promise = Promise
 mongoose
-  .connect(MONGODB_URI,
-    { useNewUrlParser: true }
-  )
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("Connected to Mongo!");
+    console.log("Connected to Mongo!")
   })
   .catch(err => {
-    console.error("Error connecting to mongo", err);
-  });
+    console.error("Error connecting to mongo", err)
+  })
 
-const server = express()
-server.use(fileUpload())
-server.use(express.static(path.join(__dirname, "public")))
-server.use(routes)
+const app = express()
+app.use(express.static(path.join(__dirname, "public")))
+app.use(routes)
 
-server.listen(8000, () => {
+app.listen(8000, () => {
   console.log("Server is up and running: http://localhost:8000")
-});
+})
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,22 +1,20 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-const projectsRoutes = require("./projects");
-const categoriesRoutes = require("./categories");
-const editorRoutes = require("./editor");
+const projectsRoutes = require("./projects")
+//const categoriesRoutes = require("./categories");
+//const editorRoutes = require("./editor")
 
 router.get("/", (req, res) => {
-  res.send({ hello: true });
-});
+  res.send({ hello: true })
+})
 
-
-router.use("/projects", projectsRoutes);
-router.use("/categories", categoriesRoutes);
-router.use("/editor", editorRoutes);
-
+router.use("/projects", projectsRoutes)
+//router.use("/categories", categoriesRoutes);
+//router.use("/editor", editorRoutes)
 
 router.use((req, res) => {
-  res.status(404).send({ error: "not-found" });
-});
+  res.status(404).send({ error: "not-found" })
+})
 
-module.exports = router;
+module.exports = router
