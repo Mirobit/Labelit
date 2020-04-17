d9fa80f933f007e4ea3564394454e3d49de746a6
Michael <13236924+Mirobit@users.noreply.github.com>
2020-04-18 00:39:08 +0200

    Fix redirect after deleting project

--- frontend/assets/js/pages/project.js
+++ frontend/assets/js/pages/project.js
@@ -90,7 +90,7 @@ const removeProject = async () => {
   if (!confirmed) return
   const result = await sendData(`/projects/${Store.project._id}`, 'DELETE')
   if (result.status === true) {
-    window.location.pathname = '/projects'
+    window.location.pathname = ''
   } else {
     displayMessage(result.status, 'Could not remove project')
   }
