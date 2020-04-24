92c41d1c2c126603b7bb490ccbc392cc2cd72d97
Michael <13236924+Mirobit@users.noreply.github.com>
2020-04-24 15:53:40 +0200

    Fix XSS Header

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   res.header('Strict-Transport-Security', 'max-age=31536000')
   res.header('X-Content-Type-Options', 'nosniff')
   res.header('Referrer-Policy', 'same-origin')
-  res.header('X-XSS-Protection', '1 mode=block')
+  res.header('X-XSS-Protection', '1; mode=block')
   res.header(
     'Content-Security-Policy',
     "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src data: 'self'"
