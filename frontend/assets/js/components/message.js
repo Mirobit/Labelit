2c51a3be6e7419ed892aeabe03ca7ca7a5c2c574
Michael <13236924+Mirobit@users.noreply.github.com>
2020-05-14 23:36:18 +0200

    Fix display message

--- frontend/assets/js/components/message.js
+++ frontend/assets/js/components/message.js
@@ -1,19 +1,19 @@
 import Store from '../store.js'
 
 const displayMessage = (success, message) => {
-  if (success) {
-    document.getElementById('messageText').innerText = message
-    Store.messageDiv.classList.add('alert-success')
-  } else {
-    document.getElementById('messageText').innerText = message
-    Store.messageDiv.classList.add('alert-warning')
-  }
   Store.messageDiv.hidden = false
+  if (success) {
+    Store.messageDiv.classList.add('alert-success')
+    Store.messageDiv.classList.remove('alert-warning')
+    document.getElementById('messageText').innerText = message
+  } else {
+    Store.messageDiv.classList.add('alert-warning')
+    Store.messageDiv.classList.remove('alert-success')
+    document.getElementById('messageText').innerText = message
+  }
 }
 
 const closeMessage = () => {
-  Store.messageDiv.classList.remove('alert-warning')
-  Store.messageDiv.classList.remove('alert-success')
   Store.messageDiv.hidden = true
 }
 
